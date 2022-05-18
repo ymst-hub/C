91489a2e974d6f26f90dc5e1186e2c89f4eb39f7
y <y>
2022-05-19 00:04:57 +0900

    コミット

---
+++ mozi2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    int sum;
+    char str[100];
+    scanf("%s",str);//scanfの最後の入力には\0がつく
+
+    for(int i = 0;str[i]!='\0';i++){//0ではない間続ける
+        sum++;
+    }
+    printf("%d",sum);
+
+    sum = strlenz(str);//string.hをincludeするとできる。やることは文字数を数える
+    printf("%d",sum);
+
+}
