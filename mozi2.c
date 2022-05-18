17c05ac85f7984bd9886b819f125452d63a25a64
y <y>
2022-05-19 00:38:24 +0900

    コミット

--- mozi2.c
+++ mozi2.c
@@ -2,16 +2,38 @@
 #include <string.h>
 
 int main(void){
-    int sum;
+    int sum,i = 0;
     char str[100];
+    char str2[100];
+    printf("最初の文字列を入力してください\n");
     scanf("%s",str);//scanfの最後の入力には\0がつく
 
-    for(int i = 0;str[i]!='\0';i++){//0ではない間続ける
-        sum++;
-    }
-    printf("%d\n",sum);
+    printf("次のの文字列を入力してください\n");
+    scanf("%s",str2);
 
+    if(sizeof(str)==sizeof(str2)){
+        printf("同じ桁です\n");
+        for(i = 0;i<sizeof(str)+1;i++){
+            if(str[i] != str2[i]) break; 
+        }
+    }else if(sizeof(str)<sizeof(str2)+1){
+        printf("後に入力した方が大きいです\n");
+        for(i = 0;i<sizeof(str2)+1;i++){
+            if(str[i] != str2[i]) break; 
+        }
+    }else{
+        printf("前に入力した方が大きいです\n");
+        for(i = 0;i<sizeof(str)+1;i++){
+            if(str[i] != str2[i]) break; 
+        }
+    }
+
+    if(strcmp(str,str2)==0){//string.hをincludeするとできる。文字の比較を行う
+        printf("同じ文字列です\n");
+    }else{
+        printf("違う文字列です\n");
+    }
     sum = strlen(str);//string.hをincludeするとできる。やることは文字数を数える
-    printf("%d\n",sum);
+    printf("文字数は%d\n",sum);
 
 }
