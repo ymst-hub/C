78da903846dedf87f67d45c92774d7f8323a5a4d
y <y>
2022-05-22 21:21:00 +0900

    コミット

--- kouzoutai.c
+++ kouzoutai.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 struct student {
     int year;//学年
@@ -6,3 +7,16 @@ struct student {
     int num;//出席番号
     char name[64];//名前
 };//;が必要
+
+int main(void){
+    struct student data;//呼び出し
+    data.year = 10;//studantのyearに学年を代入
+    strcpy(data.name,"YU");
+    printf("%d\n",data.year);
+    printf(data.name);
+
+
+    return 0;
+    
+}
+
